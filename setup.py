2cfe2598b2fd0d419c30afecbbcd6f850de445a2
jgomezz <jgomezz@gmail.com>
2024-06-12 09:19:40 -0500

    first version

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='quipudigital',
+    version='0.1.0',
+    package_data={'mathlib': ['assets/*.*']},  # Include all files in the assets folder
+    packages=find_packages(),
+)
+
+
